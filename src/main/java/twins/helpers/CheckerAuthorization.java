ce33af486e01b3808c4125666925103edd64d289
zahta <zahta@zah-tayar>
2021-05-15 19:11:18 +0300

    Check Authorization functions

--- src/main/java/twins/helpers/CheckerAuthorization.java
+++ src/main/java/twins/helpers/CheckerAuthorization.java
@@ -14,9 +14,38 @@ public class CheckerAuthorization {
 		super();
 		this.userHandler = userHandler;
 	}
-	
-	
-	
-	
+	public boolean CheckEmailValid(String email) {
+		if (email.contains("@"))
+			return true;
+		return false;
+	}
+	public boolean CheckValidUser(String space,String email) {
+		//get user from db by space and email and id 
+		//if user not exist false 
+		return true;
+		
+		
+	}
+	public boolean CheckAdminUser(String userSpace, String userEmail) {
+		//get user from db by space and email and id 
+				//if user not exist false 
+		//check if user is admin 
+		//if not -->false 
+		return true ;
+	}
+	public boolean CheckManagerUser(String userSpace, String userEmail) {
+		//get user from db by space and email and id 
+				//if user not exist false 
+		//check if user is manager 
+		//if not -->false 
+		return true ;
+	}
+	public boolean CheckPlayerUser(String userSpace, String userEmail) {
+		//get user from db by space and email and id 
+				//if user not exist false 
+		//check if user is manager 
+		//if not -->false 
+		return true ;
+	}
 
 }
